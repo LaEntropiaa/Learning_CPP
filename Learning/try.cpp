63d0073050bab38cb61964681b1d155719894a73
Alonso887 <138240277+Alonso887@users.noreply.github.com>
2024-01-27 20:49:05 -0600

    nuthing

---
+++ Learning/try.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+	int file_size = 69;
+	int counter = 100;
+	int temp = file_size;
+	file_size = counter;
+	counter = temp;
+	double sales = 9.99;
+	std::cout << counter;
+	return 0;
+}
