977c6b1d83ba5a196eef4d42c7e54ec3413f82cc
Alonso887 <138240277+Alonso887@users.noreply.github.com>
2024-01-29 09:39:45 -0600

    found a 31 hour long c++ tutorial, probably should not take it but it makes me feel smart

--- Learning/try.cpp
+++ Learning/try.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 
 int main() {
-	int file_size = 69;
-	int counter = 100;
-	int temp = file_size;
-	file_size = counter;
-	counter = temp;
-	double sales = 9.99;
-	std::cout << counter;
+	std::cout << "Alonso\n";
+	std::cout << "Alonso\n";
+	std::cout << "Alonso\n";
+	std::cout << "Alonso\n";
+	std::cout << "Alonso\n";
+	std::cout << "Alonso\n";
+	std::cout << "Alonso\n";
+	std::cout << "Alonso\n";
+	std::cout << "Alonso\n";
+	std::cout << "Alonso\n";
 	return 0;
 }
