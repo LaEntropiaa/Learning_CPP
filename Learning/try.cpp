6a2cc0ed78e617c6b008635adf40c1106ee1a3c4
Alonso887 <138240277+Alonso887@users.noreply.github.com>
2024-01-29 13:55:28 -0600

    In the learning, didn't continue the 31 hour tutorial, explains the basics of programing in general and i already konw that i just need to know c++

--- Learning/try.cpp
+++ Learning/try.cpp
@@ -1,15 +1,12 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-	std::cout << "Alonso\n";
-	std::cout << "Alonso\n";
-	std::cout << "Alonso\n";
-	std::cout << "Alonso\n";
-	std::cout << "Alonso\n";
-	std::cout << "Alonso\n";
-	std::cout << "Alonso\n";
-	std::cout << "Alonso\n";
-	std::cout << "Alonso\n";
-	std::cout << "Alonso\n";
+	double temperature;
+	cout << "Enter a farenheit temperature \n> ";
+	cin >> temperature;
+	temperature = (temperature - 32) * (static_cast<double>(5) / 9);
+	cout << "Celsius: " << temperature;
 	return 0;
 }
