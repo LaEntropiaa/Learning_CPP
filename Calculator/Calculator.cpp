53f46e629dcabdd334a01fd7002551da64684048
Alonso887 <138240277+Alonso887@users.noreply.github.com>
2024-01-31 12:20:09 -0600

    Did a little silly calculator, at this pace i will dominate c++ in 6 years (a deep sadnes is feeled while reading this mesage)

---
+++ Calculator/Calculator.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+
+double calculate(double x, double y, char operation)
+{
+    if (operation == '+')
+    {
+        return x + y;
+    }
+    else if (operation == '-')
+    {
+        return x - y;
+    }
+    else if (operation == '*')
+    {
+        return x * y;
+    }
+    else if (operation == '/')
+    {
+        return x / y;
+    }
+    else
+    {
+        throw std::invalid_argument("Erro: Operator not valid.");
+    }
+}
+
+int main()
+{
+    double num1;
+    double num2;
+    char operation;
+    std::cout << "Chose an operation\n Format: a+b|a-b|a*b|a/b\n> ";
+    std::cin >> num1 >> operation >> num2;
+    std::cout << "\n" << calculate(num1, num2, operation);
+    return 0;
+}
--- Learning/try.cpp
+++ Learning/try.cpp
@@ -1,22 +1,9 @@
 #include <iostream>
-#include <cmath>
-
-double power(double base, int exponent)
-{
-    double result = 1;
-    for (int i = 0; i < exponent; i++)
-    {
-        result = result * base;
-    }
-    return result;
-}
+#include <string>
 
 int main() 
-{
-    int base, exponent;
-    std::cout << "Entre a base and exponent:\n> ";
-    std::cin >> base >> exponent;
-    std::cout << power(base, exponent);
+{   
+    char x = '0';
+    std::cout << (int)x;
     return 0;
 }
-
