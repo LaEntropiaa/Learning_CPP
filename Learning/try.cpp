58cb02631e7a697adbd19ce0f9127978d65f7760
Alonso887 <138240277+Alonso887@users.noreply.github.com>
2024-02-01 14:06:48 -0600

    In the learning i guess

--- Learning/try.cpp
+++ Learning/try.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
-#include <string>
 
 int main() 
 {
-    std::string list = "Hi guys";
-    std::cout << list + " your mom";
+	int age = 16;
+	switch (age)
+	{
+	case 16:
+		std::cout << "Fucking idiot hahahahahah";
+		break;
+	case 17:
+		age = 19;
+		break;
+	default:
+		age = 18;
+		break;
+	}
+
     return 0;
 }
