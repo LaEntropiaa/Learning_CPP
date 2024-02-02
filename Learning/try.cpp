9747248e2c84b8ee6e16e6ebe13d3772e812d2e8
Alonso887 <138240277+Alonso887@users.noreply.github.com>
2024-02-02 12:17:24 -0600

    still learning, is becoming a habit :)

--- Learning/try.cpp
+++ Learning/try.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
+#include <cstdlib>
+#include <string>
+#include <Windows.h>
+
+#ifdef _WIN32
+#endif // _WIN32
+
 
 int main() 
-{
-	int age = 16;
-	switch (age)
-	{
-	case 16:
-		std::cout << "Fucking idiot hahahahahah";
-		break;
-	case 17:
-		age = 19;
-		break;
-	default:
-		age = 18;
-		break;
-	}
+{   
+    int answer = 34;
+    int points;
+    int guess;
+    std::cout << "Guess the number;\n> ";
+    std::cin >> guess;
+    points = guess == answer ? 10 : 0;
+    std::cout << "Points: " << points;
 
     return 0;
 }
