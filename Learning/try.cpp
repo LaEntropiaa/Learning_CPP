13bca3ab4a38a27e7b3c9df137e2b33e487f4968
Alonso887 <138240277+Alonso887@users.noreply.github.com>
2024-02-07 21:14:34 -0600

    lr

--- Learning/try.cpp
+++ Learning/try.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
 #include <fstream>
-
+#include <vector>
+#include <string>
 
 int main()
 {
     std::ofstream file ("hello.txt");
-    
+    if (file.is_open())
+    {
+        std::cout << "success n00b\n";
+    }
+    std::vector<std::string> names;
+    names.push_back("Alonso");
+    names.push_back("Kevin");
+    names.push_back("Arasha");
+    for (std::string i : names)
+    {
+        file << i << "\n";
+    }
     file.close();
 
     return 0;
